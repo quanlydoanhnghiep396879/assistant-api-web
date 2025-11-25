4ac0c21465f9e434e5a6bbd3b74844d59d5541a2
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-11-26 01:44:22 +0700

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,10 +6,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
