ed947d91ee480010aaf210bc41cbdce5fd80eda2
quanlydoanhnghiep396879 <quanlydoanhnghiep396879@gmail.com>
2025-11-26 04:19:40 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
   useEffect(() => {
     async function fetchAPI() {
       const res = await axios.post(
-        "https://vietduc.n8n.cloud/webhook/assistant-api/hourly-report"
+        "https://vietduc.app.n8n.cloud/webhook/assistant-api/hourly-report"
       );
       setData(res.data);
     }
